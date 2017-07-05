4a1a95c9a1ce714c8156c07aaa46daae55bd8c17
Johannes Schindelin <johannes.schindelin@gmx.de>
2017-07-05 17:15:55 +0200

    mingw: special-case spawnve()ing MSYS2 programs
    MSYS2 has funny quoting rules... So we have to accommodate for it by
    quoting specifically when we detect that the executable is MSYS2 (for
    which we use the tell-tale that the .exe lives in usr/bin/).
    
    As the MSYS2 quoting rules are different from the regular Win32 ones,
    and as we already handle the regular Win32 case, we now need to be
    careful to choose either one or the other.
    
    Signed-off-by: Johannes Schindelin <johannes.schindelin@gmx.de>

--- win32/process.c
+++ win32/process.c
@@ -90,12 +90,42 @@ parse_interpreter(const char *cmd, interp_t *interp)
 	return 1;
 }
 
+static char *quote_arg_msys2(const char *arg)
+{
+	int escapes = 0, has_white_space = 0;
+	const char *p;
+	char *q, *result;
+
+	for (p = arg; *p; p++)
+		if (isspace(*p))
+			has_white_space = 1;
+		else if (*p == '"' || *p == '\\')
+			escapes++;
+
+	if (!escapes && !has_white_space && p != arg)
+		return (char *)arg;
+
+	q = result = malloc(p - arg + escapes + 3);
+	if (!q)
+		return (char *)arg; /* out of memory: punt */
+	*(q++) = '"';
+	for (p = arg; *p; p++) {
+		if (*p == '"' || *p == '\\')
+			*(q++) = '\\';
+		*(q++) = *p;
+	}
+	*(q++) = '"';
+	*q = '\0';
+
+	return result;
+}
+
 /*
  * See http://msdn2.microsoft.com/en-us/library/17w5ykft(vs.71).aspx
  * (Parsing C++ Command-Line Arguments)
  */
 static char *
-quote_arg(const char *arg)
+quote_arg_mingw(const char *arg)
 {
 	int len = 0, n = 0;
 	int force_quotes = 0;
@@ -200,6 +230,25 @@ find_first_executable(const char *name)
 	return exe_path;
 }
 
+static inline int is_slash(char c)
+{
+	return c == '/' || c == '\\';
+}
+
+static int is_msys2_cmd(const char *cmd)
+{
+	int len = strlen(cmd);
+
+	if (len < 9)
+		return 0;
+
+	while (len-- && !is_slash(cmd[len]))
+		; /* do nothing */
+	return len > 7 && is_slash(cmd[len - 8]) && is_slash(cmd[len - 4]) &&
+		!_strnicmp(cmd + len - 7, "usr", 3) &&
+		!_strnicmp(cmd + len - 3, "bin", 3);
+}
+
 static intptr_t
 spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 {
@@ -208,6 +257,7 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 	int i, argc = -1;
 	intptr_t ret;
 	struct stat st;
+	char *(*quote_arg)(const char *);
 
 	/*
 	 * Require that the file exists, is a regular file and is executable.
@@ -225,6 +275,7 @@ spawnveq(int mode, const char *path, char *const *argv, char *const *env)
 		return -1;
 	}
 
+	quote_arg = is_msys2_cmd(path) ? quote_arg_msys2 : quote_arg_mingw;
 	while (argv[++argc])
 		;
 
